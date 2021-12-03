1ecda66d00ef57c242f8a64b79385abbf4adbcc3
Malcolm Mashig <mjm6jy@virginia.edu>
2021-12-03 00:32:21 -0500

    reverse refactoring a bit

--- download-librispeech-model.sh
+++ download-librispeech-model.sh
@@ -1,9 +1,7 @@
 # download-librispeech-model.sh
 
-mkdir librispeech-tools
-cd librispeech-tools
+cd docker-delivery
 wget http://kaldi-asr.org/models/13/0013_librispeech_v1_chain.tar.gz
 wget http://kaldi-asr.org/models/13/0013_librispeech_v1_extractor.tar.gz
 wget http://kaldi-asr.org/models/13/0013_librispeech_v1_lm.tar.gz
 cd ..
-docker cp $base_dir/librispeech-tools/. $docker_cid:opt/kaldi/egs/wsj/s5/librispeech-tools
